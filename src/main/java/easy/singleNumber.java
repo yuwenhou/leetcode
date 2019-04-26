2f3154736c3d4c6c2b1ef2a8401db89281f585fc
yuwen <houyuwen2014@163.com>
2019-04-26 11:18:45 +0800

    20190426

--- src/main/java/easy/singleNumber.java
+++ src/main/java/easy/singleNumber.java
@@ -26,12 +26,29 @@ public class singleNumber {
         type[] arrayName = {element1,element2,element3...};
         arrayName = new type[length];
       */
-        System.out.println(singleNumber(num));
+
+        long startTime1=System.nanoTime();   //获取开始时间
+        System.out.println("1.使用遍历:"+singleNumber1(num));
+        long endTime1=System.nanoTime(); //获取结束时间
+        System.out.println("1.使用遍历程序运行时间： "+(endTime1-startTime1)+"ns");
+
+
+        /**
+         * 异或原理
+         * 1.交换律：a ^ b ^ c <=> a ^ c ^ b
+         * 2.任何数于0异或为任何数 0 ^ n => n
+         * 3.相同的数异或为0: n ^ n => 0
+         */
+        long startTime2=System.nanoTime();   //获取开始时间
+        System.out.println("2.使用异或:"+singleNumber2(num));//测试的代码段
+        long endTime2=System.nanoTime(); //获取结束时间
+        System.out.println("2.使用异或程序运行时间： "+(endTime2-startTime2)+"ns");
+
+
+
     }
 
-    public static int singleNumber(int[] nums) {
-
-
+    public static int singleNumber1(int[] nums) {
         for (int num:nums){
             int count = 0;
             for (int i = 0; i< nums.length;i++){
@@ -42,12 +59,16 @@ public class singleNumber {
             if (count==1){
                 return num;
             }
-
         }
-
         return 0;
     }
 
-
+    public static int singleNumber2(int[] nums) {
+        int a = 0;
+        for(int num:nums){
+            a = a^num;
+        }
+        return a;
+    }
 
 }
